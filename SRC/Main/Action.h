e62a6729e4216480dc6341a444fdfdcca0ab3a59
Hamzaxx370 <hamzamhmd2008@gmail.com>
2025-06-13 13:54:45 +0300

    action.h little change

--- SRC/Main/Action.h
+++ SRC/Main/Action.h
@@ -1,57 +1,20 @@
 #include "..\Context.cpp"
-class CActMan {
-    // total size: 0x2780
-public:
-    CActMan();
-    void systemInit();
-    class CActHumanManager* GetAction(int);
-    class CActBase * m_lpRootAction; // offset 0x0, size 0x4
-    class CActBase * m_lpActExecRoot[4]; // offset 0x4, size 0x10
-    class CActBase * * m_lplpActExecPtr[4]; // offset 0x14, size 0x10
-    class CActBase * m_lpExecYetRoot[4]; // offset 0x24, size 0x10
-    class CActBase * * m_lplpExecYetPtr[4]; // offset 0x34, size 0x10
-    class CActBase * m_lpDelSameRoot; // offset 0x44, size 0x4
-    class CActBase * * m_lplpDelSamePtr; // offset 0x48, size 0x4
-    class CActBase * m_lpDelNextRoot; // offset 0x4C, size 0x4
-    class CActBase * * m_lplpDelNextPtr; // offset 0x50, size 0x4
-    class CActBase * m_pActTypeListRoot[5][16]; // offset 0x54, size 0x140
-    class CActBase * * m_ppActTypeListPtr[5][16]; // offset 0x194, size 0x140
-    unsigned int m_dwActTypeIDCount[5]; // offset 0x2D4, size 0x14
-    class CActBase * m_lpActTbl[1755]; // offset 0x2E8, size 0x1D6C
 
-    union {
-        unsigned int m_dwWork[7]; // offset 0x1E54, size 0x1C
-        signed int m_lWork[7]; // offset 0x1E54, size 0x1C
-        float m_fWork[7]; // offset 0x1E54, size 0x1C
-        void * m_lpWork[7]; // offset 0x1E54, size 0x1C
-    };
-
-    class CVisible * m_lpVisibleRoot; // offset 0x1E70, size 0x4
-    class CVisible * * m_lplpVisiblePtr; // offset 0x1E74, size 0x4
-    class CDrawBase * m_lpDrawBaseRoot[32]; // offset 0x1E78, size 0x80
-    class CDrawBase * * m_lplpDrawBasePtr[32]; // offset 0x1EF8, size 0x80
-    class CDrawBase * m_lpDrawBaseSortRoot[32]; // offset 0x1F78, size 0x80
-    class CDrawBase * * m_lplpDrawBaseSortPtr[32]; // offset 0x1FF8, size 0x80
-    class CDrawBase * m_lpDrawBaseSortedRoot[32]; // offset 0x2078, size 0x80
-    enum eACTMODE m_ActMode; // offset 0x20F8, size 0x4
-    enum eACTMODE m_ChangeActMode; // offset 0x20FC, size 0x4
-    enum eACTMODE m_SaveFileWaitActMode; // offset 0x2100, size 0x4
-    unsigned int m_dwFrameCount; // offset 0x2104, size 0x4
-    unsigned int m_dwAnimCount; // offset 0x2108, size 0x4
-    unsigned int m_dwAnimStep; // offset 0x210C, size 0x4
-    class CCamera * m_lpCurrentCamera; // offset 0x2110, size 0x4
-    unsigned int m_dwDrawSkipFrame; // offset 0x2114, size 0x4
-    unsigned int m_dwDrawSkipFrameRegist; // offset 0x2118, size 0x4
-    unsigned int m_dwInt; // offset 0x211C, size 0x4
-    unsigned int m_dwIntRegist; // offset 0x2120, size 0x4
-    float m_fActSpeedRatio; // offset 0x2124, size 0x4
-    float m_fActSpeedSquareRatio; // offset 0x2128, size 0x4
-    unsigned int m_pDrawListBuf[256]; // offset 0x212C, size 0x400
-    class CDrawBase * m_pCurrentDraw; // offset 0x252C, size 0x4
-    int GetAction(long);
-    void Init();
-    int Idle();
+enum eACTID
+{
+    eACTID_MISSION = 2,
+    eACTID_YACTMAN = 4,
+    eACTID_PLAYERCTRL = 7,
+    eACTID_PLAYER = 8,
+    eACTID_ENEMY_MANAGER = 825,
+    eACTID_ENEMY_START= 826,
+    eACTID_ENEMY_END= 858,
+    eACTID_ENEMY_CONTROL_MANAGER = 791,
+    eACTID_ENEMY_CONTROL_START = 792,
+    eACTID_ENEMY_CONTROL_END = 824,
+    eACTID_MG_BACCARA = 1749,
 };
+
 enum eACTMODE {
     eACTMODE_NORMAL = 0,
     eACTMODE_EVENT = 1,
@@ -106,6 +69,61 @@ enum eFILECATEGORY {
     eFILECATEGORY_ILLEGAL = 0x80000000,
     eFILECATEGORY_FORCE_DWORD = 0x7FFFFFFF,
 };
+
+class CActMan {
+    // total size: 0x2780
+public:
+    CActMan();
+    void systemInit();
+    class CActHumanManager* GetAction(int);
+    class CActBase * m_lpRootAction; // offset 0x0, size 0x4
+    class CActBase * m_lpActExecRoot[4]; // offset 0x4, size 0x10
+    class CActBase * * m_lplpActExecPtr[4]; // offset 0x14, size 0x10
+    class CActBase * m_lpExecYetRoot[4]; // offset 0x24, size 0x10
+    class CActBase * * m_lplpExecYetPtr[4]; // offset 0x34, size 0x10
+    class CActBase * m_lpDelSameRoot; // offset 0x44, size 0x4
+    class CActBase * * m_lplpDelSamePtr; // offset 0x48, size 0x4
+    class CActBase * m_lpDelNextRoot; // offset 0x4C, size 0x4
+    class CActBase * * m_lplpDelNextPtr; // offset 0x50, size 0x4
+    class CActBase * m_pActTypeListRoot[5][16]; // offset 0x54, size 0x140
+    class CActBase * * m_ppActTypeListPtr[5][16]; // offset 0x194, size 0x140
+    unsigned int m_dwActTypeIDCount[5]; // offset 0x2D4, size 0x14
+    class CActBase * m_lpActTbl[1755]; // offset 0x2E8, size 0x1D6C
+
+    union {
+        unsigned int m_dwWork[7]; // offset 0x1E54, size 0x1C
+        signed int m_lWork[7]; // offset 0x1E54, size 0x1C
+        float m_fWork[7]; // offset 0x1E54, size 0x1C
+        void * m_lpWork[7]; // offset 0x1E54, size 0x1C
+    };
+
+    class CVisible * m_lpVisibleRoot; // offset 0x1E70, size 0x4
+    class CVisible * * m_lplpVisiblePtr; // offset 0x1E74, size 0x4
+    class CDrawBase * m_lpDrawBaseRoot[32]; // offset 0x1E78, size 0x80
+    class CDrawBase * * m_lplpDrawBasePtr[32]; // offset 0x1EF8, size 0x80
+    class CDrawBase * m_lpDrawBaseSortRoot[32]; // offset 0x1F78, size 0x80
+    class CDrawBase * * m_lplpDrawBaseSortPtr[32]; // offset 0x1FF8, size 0x80
+    class CDrawBase * m_lpDrawBaseSortedRoot[32]; // offset 0x2078, size 0x80
+    enum eACTMODE m_ActMode; // offset 0x20F8, size 0x4
+    enum eACTMODE m_ChangeActMode; // offset 0x20FC, size 0x4
+    enum eACTMODE m_SaveFileWaitActMode; // offset 0x2100, size 0x4
+    unsigned int m_dwFrameCount; // offset 0x2104, size 0x4
+    unsigned int m_dwAnimCount; // offset 0x2108, size 0x4
+    unsigned int m_dwAnimStep; // offset 0x210C, size 0x4
+    class CCamera * m_lpCurrentCamera; // offset 0x2110, size 0x4
+    unsigned int m_dwDrawSkipFrame; // offset 0x2114, size 0x4
+    unsigned int m_dwDrawSkipFrameRegist; // offset 0x2118, size 0x4
+    unsigned int m_dwInt; // offset 0x211C, size 0x4
+    unsigned int m_dwIntRegist; // offset 0x2120, size 0x4
+    float m_fActSpeedRatio; // offset 0x2124, size 0x4
+    float m_fActSpeedSquareRatio; // offset 0x2128, size 0x4
+    unsigned int m_pDrawListBuf[256]; // offset 0x212C, size 0x400
+    class CDrawBase * m_pCurrentDraw; // offset 0x252C, size 0x4
+    int GetAction(long);
+    void Init();
+    int Idle();
+};
+
 typedef struct unk_28 {
     u32 unk0;
     u32 unk4;
@@ -169,20 +187,9 @@ class CSysActionSpeed {
     f32 GetActionSpeed(eACTSPDTYPE);
     f32 GetActionSpeedSquare(eACTSPDTYPE);
 };
+
 CActMan* D_1572A0;
+
 int D_0015727C;
+
 CSysActionSpeed* D_001572C4;
-enum eACTID
-{
-    eACTID_MISSION = 2,
-    eACTID_YACTMAN = 4,
-    eACTID_PLAYERCTRL = 7,
-    eACTID_PLAYER = 8,
-    eACTID_ENEMY_MANAGER = 825,
-    eACTID_ENEMY_START= 826,
-    eACTID_ENEMY_END= 858,
-    eACTID_ENEMY_CONTROL_MANAGER = 791,
-    eACTID_ENEMY_CONTROL_START = 792,
-    eACTID_ENEMY_CONTROL_END = 824,
-    eACTID_MG_BACCARA = 1749,
-};
